b2d33b38a7f66d710f6eb5228069637069d12c2a
SahanAtNeura <sahan@neuraintelligence.com>
2022-10-31 21:48:38 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.conf import config
+from app.routes import health, app_data, project
+
+app = FastAPI()
+app.include_router(health.router)
+app.include_router(app_data.router)
+app.include_router(project.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000/*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Get Application Settings
+settings = config.get_settings()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to QS Backend!", "status": "Alive"}
