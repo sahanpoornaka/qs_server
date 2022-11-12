4eac88b2dfa365a92471c8842c0546c87d5be10b
SahanAtNeura <sahan@neuraintelligence.com>
2022-11-12 22:50:12 +0530

    Excel Sheet Change: Column Order Changed

--- app/core/data_processor.py
+++ app/core/data_processor.py
@@ -18,24 +18,31 @@ def create_excel_sheet(record_data: Record):
 
     # Table Headers
     sheet["A5"] = "Ref ID"
-    sheet["B5"] = "Height"
+    sheet["B5"] = "Length"
     sheet["C5"] = "Width"
-    sheet["D5"] = "Breadth"
+    sheet["D5"] = "Height"
     sheet["E5"] = "Volume"
     sheet["F5"] = "Times"
     sheet["G5"] = "Total"
-    sheet["H5"] = "Remarks"
+    sheet["H5"] = "Unit"
+    sheet["I5"] = "Remarks"
+
+    # Calculate Unit
+    unit_str = "m2"
+    if record_data.element_name in ["Door", "Window"]:
+        unit_str = "unit"
 
     # Add Pin Data
     pins: Dict[str, Pin] = record_data.pins
     for idx, (ref_id, pin) in enumerate(pins.items()):
         sheet[f"A{idx + 6}"] = ref_id
-        sheet[f"B{idx + 6}"] = pin.height
+        sheet[f"B{idx + 6}"] = pin.breadth
         sheet[f"C{idx + 6}"] = pin.width
-        sheet[f"D{idx + 6}"] = pin.breadth
-        sheet[f"E{idx + 6}"] = pin.height * pin.width * pin.breadth
+        sheet[f"D{idx + 6}"] = pin.height
+        sheet[f"E{idx + 6}"] = pin.breadth * pin.width * pin.height
         sheet[f"F{idx + 6}"] = pin.times
-        sheet[f"G{idx + 6}"] = pin.height * pin.width * pin.breadth * pin.times
-        sheet[f"H{idx + 6}"] = pin.remarks
+        sheet[f"G{idx + 6}"] = pin.breadth * pin.width * pin.height * pin.times
+        sheet[f"H{idx + 6}"] = unit_str
+        sheet[f"I{idx + 6}"] = pin.remarks
 
     return workbook
